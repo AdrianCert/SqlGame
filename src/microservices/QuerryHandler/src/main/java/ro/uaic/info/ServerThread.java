d53960357c6ae9a65d2890bb5cecf3ece442b83b
JohnBordea <49273333+JohnBordea@users.noreply.github.com>
2021-06-01 23:05:02 +0300

    Querry Handler
    Microserviciu care primeste un querry si returneaza un json cu toate aceste date.

---
+++ src/microservices/QuerryHandler/src/main/java/ro/uaic/info/ServerThread.java
@@ -0,0 +1,44 @@
+package ro.uaic.info;
+
+import ro.uaic.info.HttpMessage.HttpMessageRequest;
+import ro.uaic.info.HttpMessage.HttpMessageResponse;
+import ro.uaic.info.HttpMessage.HttpMessageResponseBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Base64;
+
+public class ServerThread implements Runnable {
+
+    private Socket socket;
+    private HttpMessageRequest httpRequest;
+
+    public ServerThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            httpRequest = HttpMessageRequest.of(bufferedReader);
+            HttpMessageResponseBuilder httpMessageResponseBuilder = new HttpMessageResponseBuilder();
+            httpMessageResponseBuilder.body(httpRequest.getBody());
+            httpMessageResponseBuilder.setHttpVersion(httpRequest.getHttpVersion());
+            httpMessageResponseBuilder.constructResponse(httpRequest.getMethod(), httpRequest.getUri());
+            httpMessageResponseBuilder.send(socket);
+
+        } catch (IOException e) {
+            System.err.println("[ERROR] Server Client Thread:" + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                System.err.println("[ERROR] Socket Close: " + e.getMessage());
+            }
+        }
+    }
+}
