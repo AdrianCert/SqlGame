f552b0ae075572330ee463e5187c408dc9f6beab
Adrian Panaintescu <adrian.cert@gmail.com>
2021-06-10 22:51:23 +0300

    cors on ms-api

--- src/microservices/ms-backend/src/main/java/ro/uaic/info/ServerThread.java
+++ src/microservices/ms-backend/src/main/java/ro/uaic/info/ServerThread.java
@@ -1,6 +1,5 @@
 package ro.uaic.info;
 
-
 import ro.uaic.info.controller.*;
 import ro.uaic.info.HttpMessage.*;
 
@@ -29,25 +28,14 @@ public class ServerThread implements Runnable {
     /**
      * Collection of active controllers
      */
-    private final static Collection<Class<? extends DispatcherController>> dispatcherControllers = Stream.of(
-        UserTableController.class,
-        BoardController.class,
-        BoardMembershipController.class,
-        BoardPublishController.class,
-        HistoryController.class,
-        PaymentController.class,
-        PostController.class,
-        QuestionController.class,
-        QuestionAnswearController.class,
-        QuestionSecController.class,
-        RoleController.class,
-        SchemaLocationController.class,
-        SchemaTableController.class,
-        UserPermisionController.class,
-        UserSecController.class,
-        UserWalletController.class,
-        WalletController.class
-    ).collect(Collectors.toCollection(LinkedList::new));
+    private final static Collection<Class<? extends DispatcherController>> dispatcherControllers = Stream
+            .of(UserTableController.class, BoardController.class, BoardMembershipController.class,
+                    BoardPublishController.class, HistoryController.class, PaymentController.class,
+                    PostController.class, QuestionController.class, QuestionAnswearController.class,
+                    QuestionSecController.class, RoleController.class, SchemaLocationController.class,
+                    SchemaTableController.class, UserPermisionController.class, UserSecController.class,
+                    UserWalletController.class, WalletController.class)
+            .collect(Collectors.toCollection(LinkedList::new));
 
     /**
      * Current
@@ -56,6 +44,7 @@ public class ServerThread implements Runnable {
 
     /**
      * The constructor for ServerThread
+     * 
      * @param socket The socket connected to the client
      */
     public ServerThread(Socket socket) {
@@ -78,6 +67,10 @@ public class ServerThread implements Runnable {
             HttpMessageResponse httpResponse = dispatch(httpRequest);
             httpResponse.addHeader(HttpMessage.HEADER.CONNECTION, httpRequest.getHeader(HttpMessage.HEADER.CONNECTION));
             httpResponse.addHeader(HttpMessage.HEADER.HOST, httpRequest.getHeader(HttpMessage.HEADER.HOST));
+            httpResponse.addHeader(HttpMessage.HEADER.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+            httpResponse.addHeader(HttpMessage.HEADER.ACCESS_CONTROL_ALLOW_METHODS, "*");
+            httpResponse.addHeader(HttpMessage.HEADER.ACCESS_CONTROL_MAX_AGE, "3000");
+            httpResponse.addHeader(HttpMessage.HEADER.ACCESS_CONTROL_ALLOW_HEADERS, "*");
             // send the http response
             httpResponse.send(socket);
             bufferedReader.close();
@@ -96,8 +89,10 @@ public class ServerThread implements Runnable {
     }
 
     /**
-     * Iterate through declared DispatcherController locking for one controller responsible.
-     * The responsible controller is found by it mapping filed ( regex component): mapRegex
+     * Iterate through declared DispatcherController locking for one controller
+     * responsible. The responsible controller is found by it mapping filed ( regex
+     * component): mapRegex
+     * 
      * @param request The Http request to be handled
      * @return HttpMessageResponse returned from controller
      */
@@ -106,18 +101,15 @@ public class ServerThread implements Runnable {
         Constructor<? extends DispatcherController> constructor;
         DispatcherController dispatcherController;
         try {
-            for( Class<? extends DispatcherController> c : dispatcherControllers) {
-                if(path.matches((String) c.getField("mapRegex").get(null))) {
+            for (Class<? extends DispatcherController> c : dispatcherControllers) {
+                if (path.matches((String) c.getField("mapRegex").get(null))) {
                     constructor = c.getConstructor();
                     constructor.setAccessible(true);
                     dispatcherController = constructor.newInstance();
                     return dispatcherController.dispatch(request);
                 }
             }
-        } catch (NoSuchFieldException
-                | NoSuchMethodException
-                | IllegalAccessException
-                | InstantiationException
+        } catch (NoSuchFieldException | NoSuchMethodException | IllegalAccessException | InstantiationException
                 | InvocationTargetException e) {
             e.printStackTrace();
             return InternalServerError(e.getMessage());
@@ -130,53 +122,49 @@ public class ServerThread implements Runnable {
 
     /**
      * Generate 404 - Not Found Http Response
+     * 
      * @return HttpMessageResponse for 404 response
      */
     private HttpMessageResponse NotFound() {
         Character quo = '"';
-        return new HttpMessageResponseBuilder()
-                .status(404)
-                .header(HttpMessage.HEADER.CONTENT_TYPE, "text/json")
+        return new HttpMessageResponseBuilder().status(404).header(HttpMessage.HEADER.CONTENT_TYPE, "text/json")
                 .header(HttpMessage.HEADER.CONNECTION, httpRequest.getHeader(HttpMessage.HEADER.CONNECTION))
                 .header(HttpMessage.HEADER.HOST, httpRequest.getHeader(HttpMessage.HEADER.HOST))
                 .body(new StringBuilder().append('{')
                         // add timestamp
-                        .append(quo).append("timestamp").append(quo).append(':')
-                        .append(quo).append(new Date().toString()).append(quo).append(',')
+                        .append(quo).append("timestamp").append(quo).append(':').append(quo)
+                        .append(new Date().toString()).append(quo).append(',')
                         // add path
-                        .append(quo).append("path").append(quo).append(':')
-                        .append(quo).append(httpRequest.getUri()).append(quo).append(',')
+                        .append(quo).append("path").append(quo).append(':').append(quo).append(httpRequest.getUri())
+                        .append(quo).append(',')
                         // add status
-                        .append(quo).append("status").append(quo).append(':')
-                        .append(quo).append("404").append(quo)
+                        .append(quo).append("status").append(quo).append(':').append(quo).append("404").append(quo)
                         .append('}').toString());
     }
 
     /**
      * Generate 500 - Internal Server Error Http Response
+     * 
      * @param message The error message
      * @return HttpMessageResponse for 500 response
      */
     private HttpMessageResponse InternalServerError(String message) {
         Character quo = '"';
-        return new HttpMessageResponseBuilder()
-                .status(505)
-                .header(HttpMessage.HEADER.CONTENT_TYPE, "text/json")
+        return new HttpMessageResponseBuilder().status(505).header(HttpMessage.HEADER.CONTENT_TYPE, "text/json")
                 .header(HttpMessage.HEADER.CONNECTION, httpRequest.getHeader(HttpMessage.HEADER.CONNECTION))
                 .header(HttpMessage.HEADER.HOST, httpRequest.getHeader(HttpMessage.HEADER.HOST))
                 .body(new StringBuilder().append('{')
                         // add timestamp
-                        .append(quo).append("timestamp").append(quo).append(':')
-                        .append(quo).append(new Date().toString()).append(quo).append(',')
+                        .append(quo).append("timestamp").append(quo).append(':').append(quo)
+                        .append(new Date().toString()).append(quo).append(',')
                         // add message
-                        .append(quo).append("message").append(quo).append(':')
-                        .append(quo).append(message).append(quo).append(',')
+                        .append(quo).append("message").append(quo).append(':').append(quo).append(message).append(quo)
+                        .append(',')
                         // add path
-                        .append(quo).append("path").append(quo).append(':')
-                        .append(quo).append(httpRequest.getUri()).append(quo)
+                        .append(quo).append("path").append(quo).append(':').append(quo).append(httpRequest.getUri())
+                        .append(quo)
                         // add status
-                        .append(quo).append("status").append(quo).append(':')
-                        .append(quo).append("505").append(quo)
+                        .append(quo).append("status").append(quo).append(':').append(quo).append("505").append(quo)
                         .append('}').toString());
     }
 }
