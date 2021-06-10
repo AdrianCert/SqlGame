a4045553f321f9c156a692cd31248656f1df7f62
Dragos101 <57534351+Dragos101@users.noreply.github.com>
2021-06-10 11:10:34 +0300

    -can have a title, description, money, solution

--- src/resources/js/createQuestion.js
+++ src/resources/js/createQuestion.js
@@ -5,19 +5,24 @@ let send_body = {
     'description' : "",
     'solution' : "",
     'value' : 20,
-    'reward' : 40
+    'reward' : 0
 }
 
 const data = (ev) => {
     ev.preventDefault();
     let entity = {
-        'text' : document.getElementById("enunt").value,
-        'valid': document.getElementById("solutia").value
+        'title' : document.getElementById("titlu").value,
+        'enunt': document.getElementById("enunt").value,
+        'solutia' : document.getElementById("solutia").value,
+        'costa' : document.getElementById("costa").value
     }
     console.warn('added', {entity});
     //construiesc send_body
-    send_body["title"] = entity["text"];
-    send_body["solution"] = entity["valid"];  
+    send_body["title"] = entity["title"];
+    send_body["description"] = entity["enunt"];
+    send_body["solution"] = entity["solutia"];
+    send_body["value"] = entity["costa"];
+    send_body["reward"] = entity["costa"] * 2;
     console.warn('added', {send_body});
     //trimit request ul
     let response = fetch(url,{
