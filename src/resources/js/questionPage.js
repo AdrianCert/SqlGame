83ca4773e1f2bc2344fd2f7542f62b6279e6f666
Dragos101 <57534351+Dragos101@users.noreply.github.com>
2021-06-15 16:14:26 +0300

    +pageQuestion
    +profile card
    +clasament card
    + title/details pentru o intrebare in particular
    + profilcard dupa id-ul userului curent!

--- src/resources/js/questionPage.js
+++ src/resources/js/questionPage.js
@@ -1,8 +1,50 @@
+const url_question_by_id = "http://localhost:2021/question/{id}";
+
+function getCurrentQuestionId(){
+    return parseInt(window.location.pathname.replace("/question/", ""));
+}
+
+function getFragment(str){
+    return document.createRange().createContextualFragment(str);
+}
+
+async function getQuestionDetails(){
+    var current_id = getCurrentQuestionId();
+    var url_ = url_question_by_id.replace("{id}", current_id);
+    return await fetch(url_, {
+        method : 'GET'
+    }).then(r => r.json());
+}
+
+async function pageQuestion_replace(){
+    var details = await getQuestionDetails();
+    let container1 = document.getElementById("first_row");
+    while(container1.firstChild){
+        container1.firstChild.remove();
+    }
+    container1.appendChild(getFragment(
+        `
+        <p>${details.title}</p>
+        `
+    ));
+
+    let container2 = document.getElementById("second_row");
+    while(container2.firstChild){
+        container2.firstChild.remove();
+    }   
+    container2.appendChild(getFragment(
+        `
+        <p>${details.description}</p>
+        `
+    ));
 
 
+}
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async() => {
     console.log("DOM loaded on questionPage");
-    writeProfileCard(40);
+    let current_user_id = await didIGetIt();
+    writeProfileCard(current_user_id.id);
     writeClassamentCard();
+    pageQuestion_replace();
 });
