9599aa4c80d82ad0958b108cac0f8c305f2dff3e
Dragos101 <57534351+Dragos101@users.noreply.github.com>
2021-06-15 09:28:54 +0300

    -small changes to profileCard

--- src/resources/js/profileCard.js
+++ src/resources/js/profileCard.js
@@ -2,18 +2,15 @@ let url_user = "http://localhost:2021/user/";
 let url_wuser = "http://localhost:2021/wuser/";
 let url_wallet = "http://localhost:2021/wallet/";
 
-let question = null;
 let username = null;
 let user_coins = null;
 let poz_clasament = null;
 
-var users = {};
-var user_wallet = {};
-var wallet = {};
 var classament = [];
 
 function setUsername(data){
     username = data;
+    
 }
 
 function setUserCoins(data){
@@ -25,56 +22,10 @@ function setPozClasament(data){
 }
 
 async function load(ID){
-    await getUsers();
-    await getUWallet();
-    await getWallet();
-    makeClassament();
-    sortClassament();
+    classament = await getClassament();
+    console.log(classament);
     getInfo(parseInt(ID));
-    repalce();
-}
-
-async function getUsers(){
-    await fetch(url_user, {
-        method : 'GET'
-    }).then(r => r.json()).then(r => users = r);
-}
-
-async function getUWallet(){
-    await fetch(url_wuser, {
-        method : 'GET'
-    }).then(r => r.json()).then(r => user_wallet = r);
-}
-
-async function getWallet(){
-    await fetch(url_wallet, {
-        method: 'GET'
-    }).then(r => r.json()).then(r => wallet = r);
-}
-
-function makeClassament(){
-    var id_currentUser = 0;
-    var user_name = null;
-    var currency = 0;
-    var wallet_id = 0;
-    for(i in users){
-        id_currentUser = parseInt(users[i]['id']);
-        user_name = users[i]['user_name'];
-        currency = 0;
-        for(j in user_wallet){
-            if(parseInt(user_wallet[j]['user_id']) == id_currentUser){
-                wallet_id = user_wallet[j]['wallet_id'];
-                for(x in wallet){
-                    if(wallet[x]['id'] == wallet_id) currency = wallet[x]['balancing'];
-                }
-                classament.push({'id' : id_currentUser, 'nickname' : user_name, 'coins' : currency});
-            }
-        }
-    }
-}
-
-function sortClassament(){
-    classament.sort((a, b) => parseInt(a.coins) - parseInt(b.coins));
+    replace();
 }
 
 function getInfo(ID){
@@ -87,40 +38,7 @@ function getInfo(ID){
     }
 }
 
-function repalce(){
-    // let doc = document.createElement("body_profil");
-    // let row1 = document.createElement("row");
-    // let info1 = document.createElement("informatie");
-    // let text1_1 = document.createTextNode("* Nume utilizator:");
-    // let date1 = document.createElement("date");
-    // let text1_2 = document.createTextNode(username);
-    // doc.appendChild(row1);
-    // row1.appendChild(info1);
-    // row1.appendChild(date1);
-    // info1.appendChild(text1_1);
-    // date1.appendChild(text1_2);
-    
-    // let row2 = document.createElement("row");
-    // let info2 = document.createElement("informatie");
-    // let text2_1 = document.createTextNode("* SQLCoins:");
-    // let date2 = document.createElement("date");
-    // let text2_2 = document.createTextNode(user_coins);
-    // doc.appendChild(row2);
-    // row2.appendChild(info2);
-    // row2.appendChild(date2);
-    // info2.appendChild(text2_1);
-    // date2.appendChild(text2_2);
-    
-    // let row3 = document.createElement("row");
-    // let info3 = document.createElement("informatie");
-    // let text3_1 = document.createTextNode("*Poz. clasament:");
-    // let date3 = document.createElement("date");
-    // let text3_2 = document.createTextNode(poz_clasament);
-    // doc.appendChild(row3);
-    // row3.appendChild(info3);
-    // row3.appendChild(date3);
-    // info3.appendChild(text3_1);
-    // date3.appendChild(text3_2);
+function replace(){
     let replace1 = document.getElementById("text1");
     let replace2 = document.getElementById("text2");
     let replace3 = document.getElementById("text3");
@@ -132,7 +50,6 @@ function repalce(){
     replace1.replaceWith(text_1);
     replace2.replaceWith(text_2);
     replace3.replaceWith(text_3);
-    
 }
 
-document.addEventListener("DOMContentLoaded", () => {load(33); });
+document.addEventListener("DOMContentLoaded", () => {load(30); });
